63fa005840dd89de08a076d72c29104ce06e4ea9
boki <lsb530@naver.com>
2024-08-04 00:54:30 +0900

    init learn module

---
+++ learn/src/main/kotlin/boki/learn/LearnApplication.kt
@@ -0,0 +1,11 @@
+package boki.learn
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearnApplication
+
+fun main(args: Array<String>) {
+    runApplication<LearnApplication>(*args)
+}

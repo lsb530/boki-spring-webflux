5c2e9fbb1374f1db7a4eaf0b3244066d5086865c
boki <lsb530@naver.com>
2024-08-07 12:03:44 +0900

    step verifier - timebased-kt

---
+++ learn-kt/src/test/kotlin/boki/learn/stepverifier/timebased/StepVerifierTimeBasedTestEx3.kt
@@ -0,0 +1,28 @@
+package boki.learn.stepverifier.timebased
+
+import boki.learn.stepverifier.TimeBasedExample
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Flux
+import reactor.test.StepVerifier
+import java.time.Duration
+
+/**
+ * 검증에 소요되는 시간을 제한하는 예제
+ * - verify(Duration)을 통해 설정한 시간내에 검증이 끝나는지를 확인할 수 있다.
+ */
+class StepVerifierTimeBasedTestEx3 {
+
+    @Test
+    fun getCOVID19CountTest() {
+        StepVerifier
+            .create(
+                TimeBasedExample.getCOVID19Count(
+                    Flux.interval(Duration.ofMinutes(1)).take(1)
+                )
+            )
+            .expectSubscription()
+            .expectNextCount(11)
+            .expectComplete()
+            .verify(Duration.ofSeconds(3))
+    }
+}

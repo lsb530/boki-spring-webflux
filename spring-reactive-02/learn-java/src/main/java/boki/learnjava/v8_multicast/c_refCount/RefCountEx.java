8706dad5c60ce96c3d9681d9121c337890c84e70
boki <lsb530@naver.com>
2024-09-05 10:13:45 +0900

    refCount

---
+++ spring-reactive-02/learn-java/src/main/java/boki/learnjava/v8_multicast/c_refCount/RefCountEx.java
@@ -0,0 +1,36 @@
+package boki.learnjava.v8_multicast.c_refCount;
+
+import boki.learnjava.utils.Logger;
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+/**
+ * refCount() Operator
+ * - 다수의 Subscriber와 Flux를 공유한다.
+ * - Cold Sequence를 Hot Sequence로 변환한다.
+ * - 파라미터로 입력한 숫자만큼의 구독이 발생하는 시점에 connect()가 자동으로 호출된다.
+ * - 파라미터로 입력한 숫자만큼의 구독이 취소되면 Upstream 소스와의 연결을 해제한다.
+ * - 시퀀스 자체가 종료된다는 의미는 아님!!(어떻게보면 콜드 시퀀스하고 비슷하게 동작)
+ */
+public class RefCountEx {
+    public static void main(String[] args) throws InterruptedException {
+        Flux<Long> publisher =
+            Flux
+                .interval(Duration.ofMillis(500))
+                // .publish().autoConnect(1);
+                .publish().refCount(1);
+        Disposable disposable =
+            publisher.subscribe(data -> Logger.info("# subscriber 1: {}", data));
+
+        Thread.sleep(2100L);
+
+        // 구독이 해제 되었다면 upstream과의 연결이 해제돼서 데이터가 초기화되어야함(다시 0부터)
+        disposable.dispose();
+
+        publisher.subscribe(data -> Logger.info("# subscriber 2: {}", data));
+
+        Thread.sleep(2500L);
+    }
+}
